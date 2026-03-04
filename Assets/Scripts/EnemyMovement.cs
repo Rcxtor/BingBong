94fd1ef7313d24342c8880fd1eb787f2ed4980c5
Saif Ahmmed Sifat <sefat64@gmail.com>
2026-03-04 16:44:39 +0600

    enemy movement added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float speed = 1f;
+    private int startDirection = 1;
+    private int currentDirection;
+    private float halfWidth;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        halfWidth = spriteRenderer.bounds.extents.x;
+        currentDirection = startDirection;
+        spriteRenderer.flipX = startDirection == 1 ? false: true; // 1 = going right
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        movement.x = speed * currentDirection;
+        movement.y = rb.linearVelocityY;
+        rb.linearVelocity = movement;
+        setDirection();
+    }
+    private void setDirection()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.right, halfWidth + 0.1f, LayerMask.GetMask("Ground")) && rb.linearVelocityX > 0)
+        {
+            currentDirection *= -1;
+            spriteRenderer.flipX = true;
+        }
+        else if (Physics2D.Raycast(transform.position, Vector2.left, halfWidth + 0.1f, LayerMask.GetMask("Ground")) && rb.linearVelocityX < 0)
+        {
+            currentDirection = 1; // or *= -1
+            spriteRenderer.flipX = false;
+
+        }
+    }          
+}
